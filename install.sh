c832646f6ddbfbe1f770a89a9e642c169c0f6f19
ypkoo <ypkoo93@gmail.com>
2021-07-24 18:50:00 +0900

    errors fixed

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo yum install -y java-1.8.0-openjdk-devel.x86_64
 
 python3 create_configs.py $1
 sudo bash -c 'cat hosts >> /etc/hosts'
-. setup_zookeeper.sh
+. setup_zookeeper.sh $1
 . setup_kafka.sh
 
 
-sudo ./opt/zookeeper/bin/zkServer.sh start
-sudo ./opt/kafka/bin/kafka-server-start.sh -daemon ../config/server.properties
+# sudo /opt/zookeeper/bin/zkServer.sh start
+# sudo /opt/kafka/bin/kafka-server-start.sh -daemon /opt/kafka/config/server.properties
