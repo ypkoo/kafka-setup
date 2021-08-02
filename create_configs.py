498ed4a10b5a769f2b648d423c43ffe92e606fa8
ypkoo <ypkoo93@gmail.com>
2021-08-02 23:10:33 +0900

    mvp

--- create_configs.py
+++ create_configs.py
@@ -1,15 +1,23 @@
 #!/bin/python3
 import sys
-from host_config import broker_list
+from host_config import broker_list, broker_postfix
+
+if broker_postfix != "":
+    broker_postfix = "-" + broker_postfix
 
 myid=sys.argv[1]
 
-with open('server.properties.base', 'r') as f:
-    text = f.read().format(id=myid)
+with open('base-zoo.cfg', 'r') as f:
+    text = f.read().format(postfix=broker_postfix)
+with open('zoo.cfg', 'w') as f:
+    f.write(text)
+
+with open('base-server.properties', 'r') as f:
+    text = f.read().format(id=myid, postfix=broker_postfix)
 with open('server.properties', 'w') as f:
     f.write(text)
 
 with open('hosts', 'w') as f:
     for i, b in enumerate(broker_list):
         ip = '0.0.0.0' if i+1 == int(myid) else b
-        f.write(f'{ip} broker{i+1}\n')
+        f.write(f'{ip} broker{i+1}{broker_postfix}\n')
